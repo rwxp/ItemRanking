a3598139b577a531dc57b703a0840c95cdbe677f
Sebastian Caicedo <sdrivert@hotmail.com>
2024-01-09 13:26:34 -0500

    First commit, baseline

---
+++ RankingApp.Server/Controllers/ItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RankingApp.Server.Models;
+
+namespace RankingApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController: ControllerBase
+    {
+        private static readonly IEnumerable<ItemModel> Item         
+    }
+}
---
+++ RankingApp.Server/Models/ItemModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RankingApp.Server.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ImageId { get; set; }
+        public int Ranking {  get; set; }
+        // Movies are assigned value 1, albums are assigned value 2.
+        public int ItemType { get; set; }
+
+    }
+}
